7070b6408aaa6abdca67de773f9325d1148d44a7
vinifen <ferreiranivicius@gmail.com>
2024-12-22 21:05:57 -0300

    asdf

--- backend-pixelpost/includes/loadDotEnv.php
+++ backend-pixelpost/includes/loadDotEnv.php
@@ -3,6 +3,10 @@
 function loadDotEnv(){
   $path = dirname(__FILE__, 2);
   
+  if (!file_exists($path)) {
+    trigger_error("File .env was not found at: " . $path, E_USER_WARNING);
+  }
+
   $dotenv = Dotenv\Dotenv::createImmutable($path);
   $dotenv->load();
 }
